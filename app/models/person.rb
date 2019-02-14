e858ad7eec2be6756ded9d766ae971a1fd193bd9
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-02-14 21:07:06 +0100

    Basic PostsController#index

--- app/models/person.rb
+++ app/models/person.rb
@@ -16,4 +16,8 @@ class Person < ApplicationRecord
   
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
