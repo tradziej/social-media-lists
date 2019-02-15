769ce8e3c43ac3155949b9ae59a0b125a17accc4
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-02-15 18:13:32 +0100

    Filters on Posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.page params[:page]
+    @posts = query
+  end
+
+ private
+  def query
+    params[:page] ||= 1
+    params[:from] = Date.civil(
+      params[:from_date][:year].to_i,
+      params[:from_date][:month].to_i,
+      params[:from_date][:day].to_i
+    ) if params[:from_date].present?
+    params[:to] = Date.civil(
+      params[:to_date][:year].to_i,
+      params[:to_date][:month].to_i,
+      params[:to_date][:day].to_i
+    ) if params[:to_date].present?
+
+    query = PostQuery.new(Post.all).narrow(filters)
+
+    query.relation.page(params[:page])
+  end
+
+  def filters
+    params.permit(:from, :to, social_accounts: [], custom_lists: [])
   end
 end
--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "posts/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:person){ Person.create!(first_name: "John", last_name: "Kowalski") }
+  let(:account){ SocialMediaAccount.create!(kind: "twitter", username: "a", person: person) }
+  before(:each) do
+    assign(:posts, Kaminari.paginate_array([
+      Post.create!(
+        content: "First",
+        url: "www.",
+        person: person,
+        social_media_account: account
+      ),
+      Post.create!(
+        content: "Second",
+        url: "www.",
+        person: person,
+        social_media_account: account
+      )
+    ]).page(1))
+  end
+
+  it "displays all the posts" do
+    render
+
+    expect(rendered).to match /First/
+    expect(rendered).to match /Second/
+  end
 end
